21678c0db6c44e96557daf29ed55580db50cfb43
alanhakhyeonsong <songs4805@naver.com>
2021-06-30 23:05:34 +0900

    함수형 인터페이스와 람다 표현식 예제

--- Modern Java/src/main/java/me/ramos/java8to11/Foo.java
+++ Modern Java/src/main/java/me/ramos/java8to11/Foo.java
@@ -1,5 +1,10 @@
 package me.ramos.java8to11;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class Foo {
 
     public static void main(String[] args) {
@@ -17,7 +22,32 @@ public class Foo {
             System.out.println("Hello");
             System.out.println("Lambda");
         };*/
-        
+
         runSomething.doIt();
+
+        Plus10 plus10 = new Plus10();
+        System.out.println(plus10.apply(1));
+        // Plus10 클래스를 없애고 다음과 같이 람다 표현식으로 변경 가능
+        Function<Integer, Integer> plus10Lambda = (i) -> i + 10;
+        System.out.println(plus10Lambda.apply(10));
+
+        // compose: 매개변수부터 실행 후 전체 연산
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+        Function<Integer, Integer> multiply2AndPlus10 = plus10Lambda.compose(multiply2);
+        System.out.println(multiply2AndPlus10.apply(2));
+
+        // andThen: 순차적으로 연산
+        System.out.println(plus10Lambda.andThen(multiply2).apply(4));
+
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10);
+
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10.get());
+
+        Predicate<String> startsWithRamos = (s) -> s.startsWith("Ramos");
+        Predicate<Integer> isEven = (i) -> i%2 == 0;
+        
+
     }
 }
