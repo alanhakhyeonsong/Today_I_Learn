058f6055d7201dcb0ad176a7397a123fca80e424
alanhakhyeonsong <songs4805@naver.com>
2021-07-03 13:06:31 +0900

    내용 설명 주석 추가

--- Modern Java/src/main/java/me/ramos/java8to11/Bar.java
+++ Modern Java/src/main/java/me/ramos/java8to11/Bar.java
@@ -1,7 +1,10 @@
 package me.ramos.java8to11;
 
 public interface Bar extends Doo {
-
+    /* Bar를 구현하는 클래스에서
+    * printNameUpperCase()를 사용하고 싶지 않다면
+    * 다음과 같이 추상메소드로 둔다.
+    */
     void printNameUpperCase();
 
 }
