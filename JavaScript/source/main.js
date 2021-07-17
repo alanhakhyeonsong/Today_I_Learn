edade3b714dd21d6b49d6cd8a181590c4fa93510
alanhakhyeonsong <songs4805@naver.com>
2021-07-17 15:31:32 +0900

    javascript

---
+++ JavaScript/source/main.js
@@ -0,0 +1,6 @@
+// Whole-script strict mode syntax
+// JavaScript is very flexible
+// flexible === dangerous
+// added ECMAScprit 5
+'use strict';
+console.log("Hello World!");
---
+++ JavaScript/source/variable.js
@@ -0,0 +1,111 @@
+// 1. Use strict
+// added in ES 5
+// use this for Valina Javascript.
+'use strict';
+
+// 2. Variable
+// let (added in ES6)
+let globalName = 'global name';
+{
+  let name = 'ramos';
+  console.log(name);
+  name = 'hello';
+  console.log(name);
+  console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+console.log(age);
+age = 4;
+console.log(age);
+var age;
+
+{
+  age2 = 6;
+  var age2;
+}
+console.log(age2);
+
+// 3. Constant
+// favor immutable data type always for a few reasons:
+// - security
+// - thread safety
+// - reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// 4. Variable types
+// primitive, single item: number, string, boolean, null, undefine, symbol
+// object, box container
+// function, first-class function
+const count = 17; // integer
+const size = 17.1; // decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// number - specila numeric values: infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet)
+const bigInt = 1234567890123456789012345678901234567890n; // over (-2**53) ~ 2**53
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+Number.MAX_SAFE_INTEGER;
+
+// string
+const char = 'c';
+const ramos = 'ramos';
+const greeting = 'hello ' + ramos;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloRonaldo = `hi ${ramos}!`; // template literals (string)
+console.log(`value: ${helloRonaldo}, type: ${typeof helloRonaldo}`);
+console.log('value: ' + helloRonaldo, ' type: ' + typeof helloRonaldo);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2);
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+
+// object, real-life object, data structure
+const song = { name: 'song', age = '26'};
+song.age = 19;
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); // h
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
+console.log(text.charAt(0));
