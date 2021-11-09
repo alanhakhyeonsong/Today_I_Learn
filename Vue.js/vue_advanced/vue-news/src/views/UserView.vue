131f0970c41bffb58b5daa88b6cbfbede9554285
alanhakhyeonsong <songs4805@naver.com>
2021-11-09 21:54:26 +0900

    router, askview, userinfo

--- Vue.js/vue_advanced/vue-news/src/views/UserView.vue
+++ Vue.js/vue_advanced/vue-news/src/views/UserView.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-      user
+      <p>name: {{ userInfo.id }}</p>
+      <p>karma: {{ userInfo.karma }}</p>
+      <p>created: {{ userInfo.created }}</p>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    }
+  },
+  created() {
+    const userName = this.$route.params.id;
+    //axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
+    this.$store.dispatch('FETCH_USER', userName);
+  }
 }
 </script>
 
